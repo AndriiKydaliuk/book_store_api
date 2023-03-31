3a96b1dff42e0a8d53acc99285b6176e43e610f7
Andrii_Kydaliuk <Andrii_Kydaliuk@epam.com>
2023-03-31 10:48:02 +0300

    workflow yaml

--- Tests/UITests/conftest.py
+++ Tests/UITests/conftest.py
@@ -19,16 +19,6 @@ def app():
 
 
 @pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
-
-
-@pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
     yield driver
